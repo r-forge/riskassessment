785bc9a7e8095776eaff2cec22a6aaf5583e2867
rpouillot (Regis Pouillot) <rpouillot@yahoo.fr>
2008-11-15 03:21:00 +0000

    Add Vignettes to the www Debug mcprobtree Improve docmcEnglish
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/riskassessment@19 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/mc2d/R/mcprobtree.R
+++ pkg/mc2d/R/mcprobtree.R
@@ -1,13 +1,13 @@
 #<<BEGIN>>
-mcprobtree <- function(mcswitch, mcvalues, type=c("V","U","VU","0"), nsv=ndvar(), nsu=ndunc(),nvariates=1, outm="each", seed=NULL)
+mcprobtree <- function(mcswitch, mcvalues, type=c("V","U","VU","0"), nsv=ndvar(), nsu=ndunc(), nvariates=1, outm="each", seed=NULL)
 #TITLE Creates a Stochastic mcnode Object using a Probability Tree
 #DESCRIPTION
-#If one node should be built using a mixed of \code{mcstoc} functions/nodes, \code{mcprobtree} creates this \code{\link{mcnode}}
-#object from one node specifying the \code{mcstoc} functions/nodes to pick and a list of the different \code{mcstoc} functions/nodes.
+#This function builds a \code{mcnode} as a mixture of \code{mcstoc} functions or \code{mcnode} objects.
 #KEYWORDS methods
 #INPUTS
-#{mcswitch}<<A \code{mcnode} including the \code{mcstoc} functions/mcnodes to pick.>>
-#{mcvalues}<<A named list of \code{mcnode}, \code{mcdata} functions or \code{mcstoc} functions, or a combination of those objects.>>
+#{mcswitch}<<A vector of probabilities/weights or a \code{mcnode} including the \code{mcstoc} functions/mcnodes to pick.>>
+#{mcvalues}<<A named list of \code{mcnode}, \code{mcdata} functions or \code{mcstoc} functions, or a combination of those objects.
+#Each element should lead to an \code{mcnode} of type \code{type} and of dimension \code{c(nsv x nsu x 1)} or \code{c(nsv x nsu x nvariates)} >>
 #[INPUTS]
 #{type}<<The type of \code{mcnode} to be built. By default, a \code{"V"} node. see \code{\link{mcnode}} for details.>>
 #{nsv}<<The number of simulations in the variability dimension of the final node.>>
@@ -19,11 +19,13 @@ mcprobtree <- function(mcswitch, mcvalues, type=c("V","U","VU","0"), nsv=ndvar()
 #An \code{mcnode} object.
 #DETAILS
 #\code{mcswitch} may be:
+#{*}<<a vector of length the length of \code{mcvalues}. They need not sum to one, but they should be nonnegative and not all zero.
+#In that case, each elements of \code{mcvalues} will appear in the final sample a random number of times as specified by this vector.>> 
 #{*}<<a \code{"0"} mcnode to build any type of node.>>
 #{*}<<a \code{"V"} mcnode to build a \code{"V"} or a \code{"VU"} mcnode.>>
 #{*}<<a \code{"U"} mcnode to build a \code{"U"} or a \code{"VU"} mcnode.>>
 #{*}<<a \code{"VU"} mcnode to build a \code{"VU"}.>>
-#\code{mcswitch} will be recycled correctly if needed. (see \code{\link{mcdata}})
+#
 #
 #The elements in \code{mcvalues} should be of same type and dimension
 #as specified in \code{type}, \code{nsv}, \code{nsu} and \code{nvariates}. The name
@@ -33,11 +35,23 @@ mcprobtree <- function(mcswitch, mcvalues, type=c("V","U","VU","0"), nsv=ndvar()
 #the element will not be evaluated.
 #
 #EXAMPLE
-## A mixture of normal (Prob=0.75) and uniform (Prob=0.25)
-#conc1 <- mcstoc(rnorm,type="U",mean=10,sd=2)
-#conc2 <- mcstoc(runif,type="U",min=8,max=12)
-#whichdist <- mcstoc(rbern,type="U",prob=0.25) 
-#concbis <- mcprobtree(whichdist,list("0"=conc1,"1"=conc2),type="VU")
+### A mixture of normal (prob=0.75), uniform (prob=0.20) and constant (prob=0.05)
+#conc1 <- mcstoc(rnorm,type="VU",mean=10,sd=2)
+#conc2 <- mcstoc(runif,type="VU",min=-6,max=-5)
+#conc3 <- mcdata(0,type="VU")
+## Randomly in the cells 
+#whichdist <- mcstoc(rempiricalD,type="VU", values=1:3, prob= c(.75,.25,.05)) 
+#mcprobtree(whichdist,list("1"=conc1,"2"=conc2,"3"=conc3),type="VU")
+## Which is equivalent to 
+#mcprobtree(c(.75,.20,.05),list("1"=conc1,"2"=conc2,"3"=conc3),type="VU")
+#
+### Randomly by colums (Uncertainty) 
+#whichdist <- mcstoc(rempiricalD,type="U", values=1:3, prob= c(.75,.25,.05)) 
+#mcprobtree(whichdist,list("1"=conc1,"2"=conc2,"3"=conc3),type="VU")
+#
+### Randomly by line (Variability) 
+#whichdist <- mcstoc(rempiricalD,type="V", values=1:3, prob= c(.75,.25,.05)) 
+#mcprobtree(whichdist,list("1"=conc1,"2"=conc2,"3"=conc3),type="VU")
 #SEE ALSO
 #\code{\link{mcdata}}, \code{\link{mcstoc}}, \code{\link{switch}}.
 #EXAMPLE
@@ -46,35 +60,59 @@ mcprobtree <- function(mcswitch, mcvalues, type=c("V","U","VU","0"), nsv=ndvar()
 #--------------------------------------------
 {
 
+  if(!is.character(outm)  || (outm != "none" && outm != "each" && !all(sapply(outm,exists,mode="function"))))
+      stop("outm should be 'none','each' or the name a valid function")
+
   type <- match.arg(type)
-  if(!inherits(mcswitch,"mcnode")) stop("mcswitch should be an mcnode")
 
-  stoc <- as.list(substitute(mcvalues))
-  choixstoc <- as.numeric(names(stoc))                                          # OK for logical
-  stoc <- substitute(mcvalues)
-  choixswitch <- unique(mcswitch)
-  if(!all(choixswitch %in% choixstoc)) stop("some values of mcswitch are not present in mcvalues")
-
-  typem <- attr(mcswitch,"type")
-
-  if((type == "V" && typem == "U" )   ||
-     (type == "U" && typem == "V" )   ||
-     (type != "VU" && typem == "VU" ) ||
-     (type == "0" && typem != "0" ))     stop("Incompatible type and type of mcswitch")
+  ndunc
+  ndvar
+  if(type=="V") nsu <- 1 else if(type=="U") nsv <- 1
   
-  mcswitch <- mcdata(mcswitch,type=type,nsv=nsv,nsu=nsu,nvariates=nvariates)
+  stoc <- as.list(substitute(mcvalues)) 
+  choixstoc <- as.numeric(names(stoc)) 
+  if(any(is.na(choixstoc[-1]))) stop("Names of the mcvalues element should be convertible as numeric")  
+  stoc <- substitute(mcvalues)
+
+  if(inherits(mcswitch,"mcnode")){ 
+    typem <- attr(mcswitch,"type")
+    if((type == "V" && typem == "U" )   ||
+       (type == "U" && typem == "V" )   ||
+       (type != "VU" && typem == "VU" ) ||
+       (type == "0" && typem != "0" ))     stop("Incompatible type and type of mcswitch")
+  mcswitch <- mcdata(mcswitch, type=type, nsv=nsv, nsu=nsu, nvariates=nvariates)
+  } 
+  else 
+  if(is.vector(mcswitch)){
+    if(length(mcswitch) != length(mcvalues)) stop("the vector mcswitch should have the same length as mcvalues")
+    if(any(mcswitch < 0) || sum(mcswitch)==0 || any(!is.finite(mcswitch))) stop("mcswitch values should be finite, nonnegative and not all zero")
+    mcswitch <- mcswitch / sum(mcswitch)
+    dimf <- prod(c(nsv,nsu,nvariates))
+    mcswitch <- sample(x=choixstoc[-1], size=dimf,replace=TRUE,prob=mcswitch)}  #because stoc[[1]] is "list" 
+  else stop("mcswitch should be an mcnode")
+
+  choixswitch <- unique(mcswitch)
+  if(!all(choixswitch %in% choixstoc)) stop("some values of mcswitch are not a name of elements of mcvalues")
+
   res <- mcdata(NA,type=type,nsv=nsv,nsu=nsu,nvariates=nvariates)
 
   for(i in choixswitch){
     whichswitch <- mcswitch == i
     nbcall <- which(choixstoc == i)
-    thecall <- eval(stoc[[nbcall]])[whichswitch]                                          #because stoc[[1]] is list
-    res[whichswitch] <- thecall
+    thecall <- eval(stoc[[nbcall]])                                          
+
+    if(!inherits(thecall,"mcnode")) stop("One mcvalues does not lead to a mcnode")
+    if(attr(thecall,"type") != type) stop("One mcvalues does not lead to a mcnode of type=type")
+    dimm <- dim(thecall)
+    if(dimm[1]!=nsv || dimm[2] != nsu || (dimm[3] != 1 && dimm[3] != nvariates)) stop("One mcvalues leads to an mcnode of incoherent dimension")
+    if(dimm[3]!=nvariates) thecall <- rep(thecall,nvariates)
+    res[whichswitch] <- thecall[whichswitch]
     }
   class(res) <- "mcnode"
   attr(res,"type") <- type
   attr(res,"outm") <- outm
-  return(res)}
+  return(res)
+}
 
 
 
