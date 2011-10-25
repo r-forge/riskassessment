e7079e65797c04794e6dd6ba4c7db81cde9a0b30
rpouillot (Regis Pouillot) <rpouillot@yahoo.fr>
2011-10-25 01:02:47 +0000

    New version that should be able to pass the 2.14 tests
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/riskassessment@145 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/mc2d/R/zzz.r
@@ -0,0 +1,7 @@
+.onAttach <- function(lib, pkg)
+{
+		cat(paste("\nWelcome in mc2d. Please check new updates regularly and/or\nregister to the mailing list:\nriskassessment-news@lists.r-forge.r-project.org\n"))
+	
+}
+
+
