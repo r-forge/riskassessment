8acf2b997ddb56b73989486a7603d9186908f9dc
jbdenis <jbdenis@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2014-09-09 10:37:42 +0000

    suppression des quatre vieux paquets
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/riskassessment@277 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/g4n/R/zzz.R
+++
@@ -1,9 +0,0 @@
-.First.lib <- function(libname, pkgname) {
-    # 
-    # 10_02_03
-    #
-    # introducing the initial constants
-    #
-    rbsb3k("reset");
-    g4n3k("reset");
-}
--- pkg/rebastaba/R/zzz.R
+++
@@ -1,8 +0,0 @@
-.First.lib <- function(libname, pkgname) {
-    # 
-    # 10_02_03 10_09_24
-    #
-    # introducing the initial constants
-    #
-    rebastaba3k("RESET");
-}
