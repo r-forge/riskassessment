3df54b1091bcba541e37b26fd892ae5474dedb92
jbdenis (M. ) <jjbdenis@gmail.com>
2014-09-17 09:07:35 +0000

    14_09_17: mise à jour de rbsa
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/riskassessment@281 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/rbsa/inst/rbsa1.test.r
@@ -0,0 +1,11 @@
+#
+# 14_08_05
+#
+library(rbsa);
+#
+look <- FALSE;
+form3display(form3numbering(3,type="n"),pau=look);
+form3display(form3numbering(3,type="a"),pau=look);
+#
+form3title("rbsa1.test.r finished ",8);
+
