0022fdd65d4e9b60ac227ffb2bdbc55148d169dd
mldelignette <mldelignette@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2015-02-23 14:59:58 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/riskassessment@306 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/pkg-for-CRAN.R
+++ pkg/pkg-for-CRAN.R
@@ -3,13 +3,14 @@
 # CREATION DE PACKAGE
 
 #library(rstudioapi)
-library(roxygen2)
+# library(roxygen2)
 library(devtools)
 
-if(length(list.files("~/Desktop/Indicateurs/")) > 0) #le chemin qui t interesse
+if(length(list.files("~/MLboulot/programmes/qRa/fitdistrpack/travail_fitdistrplus")) > 0)
 {
-    print("a completer")
+  setwd("~/MLboulot/programmes/qRa/fitdistrpack/travail_fitdistrplus")
 }
+
 if(length(list.files("~/Documents/recherche-enseignement/code/R/riskassessment/riskassessment/pkg/")) > 0)
 {
     setwd("~/Documents/recherche-enseignement/code/R/riskassessment/riskassessment/pkg/")
